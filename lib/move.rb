5a5366c54f3c71b01a6a1aa45183fc8fc214d1cc
Ermite Rikudo <opouth@hotmail.fr>
2020-05-12 00:19:02 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,4 +7,27 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+display_board(board)
+
 # code your input_to_index and move method here!
+
+puts "Welcome to Tic Tac Toe!"
+puts "Where would you like to go?"
+position = gets.strip
+token = "X"
+
+while position.to_i > 9 || position.to_i <= 0 do
+  puts "Choose a position between 1 and 9 please!"
+end
+
+board[position.to_i - 1] = token
+
+display_board(board)
+
+def input_to_index
+
+end
+
+def move
+
+end
