e39f7adf4e42be5e6047bf8f64fad2574e396c99
Ermite Rikudo <opouth@hotmail.fr>
2020-05-12 02:06:41 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,7 @@ display_board(board)
 # code your input_to_index and move method here!
 #puts "Welcome to Tic Tac Toe!"
 #puts "Where would you like to go? Choose a position between 1 and 9 please!"
-def input_to_index(user_input.to_i)
+def input_to_index(user_input)
 
 end
 #input_to_index
